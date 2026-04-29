257edda5ab0a701e4834663bcb1f5565a4297a55
Oleh Musurivskyi <musurivskiioleg@gmail.com>
2026-04-30 01:19:52 +0300

    Solution

--- src/api/views.py
+++ src/api/views.py
@@ -56,3 +56,9 @@ class TodoViewSet(viewsets.ModelViewSet):
         user = self.request.user
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
+
+def health(request):
+    return HttpResponse(status=200)
+
+def readiness(request):
+    return HttpResponse(status=200)
